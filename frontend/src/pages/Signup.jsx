7bb8be9c6f0cef323d88bf28f0456309dc3ec5a0
soumenghorai <115658876+soumenghorai@users.noreply.github.com>
2025-05-19 20:28:29 +0530

    Update Signup.jsx

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -26,7 +26,7 @@ function Signup() {
       return handleError("name, email and password are required");
     }
     try {
-      const url = `https://deploy-mern-app-1-api.vercel.app/auth/signup`;
+      const url = `https://login-app-api-kappa.vercel.app//auth/signup`;
       const response = await fetch(url, {
         method: "POST",
         headers: {
