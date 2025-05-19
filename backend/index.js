c483a217b089cc69df3ec554dfb6693ba82c749c
Soumen Ghorai <soumenghorai2802@gmail.com>
2025-05-19 19:32:57 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+require("./Models/db");
+const AuthRouter = require("./Routes/AuthRouter");
+const ProductRouter = require("./Routes/ProductRouter");
+
+const PORT = process.env.PORT || 8080;
+
+app.get("/ping", (req, res) => {
+  res.send("pong");
+});
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/auth", AuthRouter);
+app.use("/products", ProductRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
